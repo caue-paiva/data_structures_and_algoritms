72e1020d41858ca71764b036102888af0fc6ad55
“Caue <cauepaivalira@outlook.com>
2023-09-25 11:23:34 -0300

    lista circular

--- lista/lista_dupla_circu.c
+++ lista/lista_dupla_circu.c
@@ -13,7 +13,7 @@ struct no{
 
 
 typedef struct{
-    NO*  fim;
+    NO*  senti;
     NO* comeco;
     int tam;
 } lista;
@@ -23,8 +23,9 @@ lista* lista_criar(){
     if (F1==NULL){exit(1);}
     
     F1->tam = 0;
-    F1->fim =NULL;
+    
     F1->comeco =NULL;
+    F1->senti=NULL;
     return F1;
 }
 
@@ -44,14 +45,19 @@ bool lista_adicionar(F_type item, lista* lista){
     NO* novo_no = no_criar(item);
 
     if(lista->tam == 0){
-        //printf("primeiro item\n");
-        lista->fim = novo_no;
+        //printf("primeiro item\n"); 
         lista->comeco = novo_no;
+        lista->senti = novo_no;
+        lista->comeco->ante = lista->senti;
+        lista->senti->prox = lista->comeco;
     } else{
        // printf("item não primeiro \n");
-        lista->fim->prox = novo_no;
-        novo_no->ante = lista->fim;
-        lista->fim = novo_no;
+        lista->senti->prox = novo_no;
+        novo_no->ante = lista->senti;
+        lista->senti = novo_no;
+        lista->senti->prox = lista->comeco;
+        lista->comeco->ante =  lista->senti;
+
     }
     ++(lista->tam);
     return true;
@@ -61,13 +67,16 @@ void lista_printar_come(lista* lista){
      if (lista->tam == 0){return;}
      NO* aux;
      aux = lista->comeco;
-     //printf("primeiro valor %d \n", aux->item);
-     do{
+     //printf("primeiro valor %d \n", aux->item);     
+     do {
         F_type item = (aux->item);
         printf("%d \n", item);
        // printf("%d\n", lista->tam);
         aux = aux->prox;
-     }while(aux!= NULL);
+
+     } while(aux !=lista->comeco);
+     
+ 
 }
 
 bool lista_remover(F_type item, lista* lista){
@@ -83,18 +92,20 @@ bool lista_remover(F_type item, lista* lista){
             NO* aux_ante = aux->ante;
             NO* aux_prox = aux->prox;
            
-            if(aux->prox != NULL){
+            if(aux->prox != lista->comeco){
               aux->prox->ante = aux_ante;
             } else{
-              aux_ante->prox=NULL;
-              lista->fim = aux_ante;
+              aux_ante->prox= lista->comeco;
+              lista->senti = aux_ante;
+              lista->senti->prox = lista->comeco;
             }
 
-            if (aux->ante != NULL){
+            if (aux->ante != lista->senti){
                aux_ante->prox = aux->prox;
             }else{
-                aux->prox->ante = NULL;
+                aux->prox->ante = lista->senti;
                 lista->comeco = aux->prox;
+                lista->comeco->ante = lista->senti;
             }
               
             free(aux);
@@ -109,18 +120,26 @@ bool lista_remover(F_type item, lista* lista){
 void lista_printar_fim(lista* lista){
      if (lista->tam == 0){return;}
      NO* aux;
-     aux = lista->fim;
+     aux = lista->senti;
+     bool prim_iter = true;
      //printf("primeiro valor %d \n", aux->item);
+    
      do{
         F_type item = (aux->item);
-        printf("%d \n", item);
-       // printf("%d\n", lista->tam);
-        aux = aux->ante;
-     }while(aux!= NULL);
+         printf("%d \n", item);
+         //printf("%d\n", lista->tam);
+         aux = aux->ante;
+     }while(aux != lista->senti);
+    
 }
 
 void lista_apagar(lista**lista){
-    if ((*lista)->tam == 0){return;}
+    if ((*lista)->tam == 0){
+        free(*lista);
+        *lista = NULL;   
+        return;
+    
+    }
      NO* aux;
      NO* aux2;
      aux = (*lista)->comeco;
@@ -129,7 +148,7 @@ void lista_apagar(lista**lista){
         free(aux);
         aux = aux2;
         aux2 = aux2->prox;
-     }while(aux2!= NULL);
+     }while(aux2 != (*lista)->comeco);
     
      free(*lista);
      *lista = NULL;
@@ -143,12 +162,14 @@ lista_adicionar(3,F1);
 lista_adicionar(6,F1);
 lista_adicionar(7,F1);
 lista_adicionar(8,F1);
-lista_printar_fim(F1);
-printf("\n\n");
-
-lista_remover(8, F1);
 
 lista_printar_come(F1);
+
+lista_remover(8, F1);
+lista_remover(3, F1);
+printf("\n");
+lista_printar_fim(F1);
+
 
 lista_apagar(&F1);
 
